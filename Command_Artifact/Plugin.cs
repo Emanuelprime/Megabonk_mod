9856cf394c0a094fd1c829e47fd893a46a3e43e2
Emanuel de Cristo Santos <141081170+Emanuelprime@users.noreply.github.com>
2025-10-13 00:26:15 -0300

    Add files via upload

---
+++ Command_Artifact/Plugin.cs
@@ -0,0 +1,29 @@
+﻿using BepInEx;
+using BepInEx.Unity.IL2CPP;
+using BepInEx.Logging;
+// using HarmonyLib;
+
+namespace Command_Artifact
+{
+    [BepInPlugin(GUID, MODNAME, VERSION)]
+    public class Plugin : BasePlugin
+    {
+        public const string
+            MODNAME = "Command_Artifact",
+            AUTHOR = "Prime_Purpura",
+            GUID = AUTHOR + "_" + MODNAME,
+            VERSION = "0.1.0";
+
+        public Plugin()
+        {
+            log = Log;
+        }
+
+        public override void Load()
+        {
+            log.LogInfo($"Loading to testando {MODNAME} v{VERSION} by {AUTHOR}");
+        }
+
+        public static ManualLogSource log;
+    }
+}
